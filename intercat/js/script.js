e6a9101446ed37e9b1c5edd960193c0676d13d40
Yruy <119973906+Yruy83@users.noreply.github.com>
2024-09-24 01:24:56 +0300

    Delete intercat directory

--- intercat/js/script.js
+++
@@ -1,8 +0,0 @@
-
-
-document.addEventListener('click', function (e) {
-	const targetElement = e.target;
-	if (targetElement.closest('.burger-menu')) {
-		document.documentElement.classList.toggle('menu-open');
-	}
-})
